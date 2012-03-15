48986c39bccbe2862e5c84c081dbbf8b2cf55012
James Sullins <jcsullins@gmail.com>
2012-03-15 00:20:31 -0500

    update 13107/7 to 13107/8, replace 12877/1 by 13629/1

--- apply-cherry_picks.sh
+++ apply-cherry_picks.sh
@@ -18,19 +18,11 @@ echo ""
 
 
 # Temporary commit for working qcom OMX decoding
-echo ">>> START 13107/7"
+echo ">>> START 13107/8"
 pushd frameworks/base
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_frameworks_base refs/changes/07/13107/7 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_frameworks_base refs/changes/07/13107/8 && git cherry-pick FETCH_HEAD
 popd
-echo ">>> END 13107/7"
-echo ""
-
-# add headers, PRODUCT_PACKAGES for OMX
-echo ">>> START 12877/1"
-pushd device/hp/tenderloin
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_hp_tenderloin refs/changes/77/12877/1 && git cherry-pick FETCH_HEAD
-popd
-echo ">>> END 12877/1"
+echo ">>> END 13107/8"
 echo ""
 
 # Added MSM vidc headers (latest from CAF)
@@ -39,6 +31,14 @@ pushd device/hp/tenderloin
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_hp_tenderloin refs/changes/62/12862/2 && git cherry-pick FETCH_HEAD
 popd
 echo ">>> END 12862/1"
+echo ""
+
+# add PRODUCT_PACKAGES for OMX
+echo ">>> START 13629/1"
+pushd device/hp/tenderloin
+git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_hp_tenderloin refs/changes/29/13629/1 && git cherry-pick FETCH_HEAD
+popd
+echo ">>> END 13629/1"
 echo ""
 
 echo ">>> START 12960/1"
